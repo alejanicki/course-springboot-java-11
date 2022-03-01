ace3ea0dd8614f3eecace3a41ffe7d49f74b228b
Alessandro Janicki Araujo <alessandrojanicki@hotmail.com>
2022-03-01 14:54:11 -0300

    Payment, one-to-one association

--- src/main/java/com/alejanicki/course/config/TestConfig.java
+++ src/main/java/com/alejanicki/course/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.alejanicki.course.entities.Category;
 import com.alejanicki.course.entities.Order;
 import com.alejanicki.course.entities.OrderItem;
+import com.alejanicki.course.entities.Payment;
 import com.alejanicki.course.entities.Product;
 import com.alejanicki.course.entities.User;
 import com.alejanicki.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
