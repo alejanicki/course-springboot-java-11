aa726bc7aa76c12a8990489801d70cc68753febd
Alessandro Janicki Araujo <alessandrojanicki@hotmail.com>
2022-03-01 12:28:59 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/alejanicki/course/entities/User.java
+++ src/main/java/com/alejanicki/course/entities/User.java
@@ -1,11 +1,19 @@
 package com.alejanicki.course.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
 
     @Id
@@ -15,6 +23,10 @@ public class User {
 	private String email;
 	private String phone;
 	private String password;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -66,6 +78,10 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
---
+++ src/main/java/com/alejanicki/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.alejanicki.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.alejanicki.course.entities.Order;
+import com.alejanicki.course.repositories.OrderRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
